739deab8381c776513644da0aac41e45814701c1
Roman <140193303+Belxz777@users.noreply.github.com>
2024-10-04 15:25:33 +0400

    jj

---
+++ firstCppVsApp.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+
+using namespace std;
+int main()
+{
+
+    string mult;
+
+
+    //механика прослушивания клавиш
+    bool isListenning = true;
+    while (isListenning) {
+        if (GetKeyState(VK_SHIFT) & 0x8000) {
+            cout << "ENTER PRESSED";
+            isListenning = false;
+        }
+    }
+    constexpr size_t  size = 10;
+    int digits[size]{ 0 };
+    for (int i = 0; i <= size; ++i) {
+
+    cin >>
+    }
+    return 0;
+}
+
